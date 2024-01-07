010cd8afc15ada003df3821f071f3af8788b4992
Alex Casquete <acasquete@plainconcepts.com>
2024-01-07 02:38:20 -0800

    Fix recalc removing rotation

--- js/taskboard.js
+++ js/taskboard.js
@@ -9,7 +9,7 @@
             showHelp();
         }
 
-        createNotes(notes);
+        initNotes(notes);
 
         screenwidth = Config.getScreenWidth();
 
@@ -49,9 +49,9 @@
         $('#toggleNotes').click(function() {
             $('.note').each(function() {
                 if ($(this).css('display') === 'none') {
-                    $(this).fadeIn(); // Show the note if it's hidden
+                    $(this).fadeIn();
                 } else {
-                    $(this).fadeOut(); // Hide the note if it's visible
+                    $(this).fadeOut();
                 }
             });
         });
@@ -60,21 +60,21 @@
             if (!document.fullscreenElement) {
                 if (document.documentElement.requestFullscreen) {
                     document.documentElement.requestFullscreen();
-                } else if (document.documentElement.mozRequestFullScreen) { /* Firefox */
+                } else if (document.documentElement.mozRequestFullScreen) {
                     document.documentElement.mozRequestFullScreen();
-                } else if (document.documentElement.webkitRequestFullscreen) { /* Chrome, Safari y Opera */
+                } else if (document.documentElement.webkitRequestFullscreen) {
                     document.documentElement.webkitRequestFullscreen();
-                } else if (document.documentElement.msRequestFullscreen) { /* IE/Edge */
+                } else if (document.documentElement.msRequestFullscreen) {
                     document.documentElement.msRequestFullscreen();
                 }
             } else {
                 if (document.exitFullscreen) {
                     document.exitFullscreen();
-                } else if (document.mozCancelFullScreen) { /* Firefox */
+                } else if (document.mozCancelFullScreen) {
                     document.mozCancelFullScreen();
-                } else if (document.webkitExitFullscreen) { /* Chrome, Safari y Opera */
+                } else if (document.webkitExitFullscreen) {
                     document.webkitExitFullscreen();
-                } else if (document.msExitFullscreen) { /* IE/Edge */
+                } else if (document.msExitFullscreen) {
                     document.msExitFullscreen();
                 }
             }
@@ -90,18 +90,18 @@
     function showHelp() {
         
         const htmlContent = `
-        <div style="text-align: center;">
-    You're the best!<br/>
-    Thanks for trying <b>TaskMe</b>,<br/>
-    the <em>easiest</em> way to create notes!
-</div>
-<br/>
-- <b>Create</b> a note by selecting a color on the left of the screen.<br/>
-- <b>Edit</b> a note by double-clicking on it.<br/>
-- <b>Remove</b> a note by dragging it to the top of the screen.<br/><br/>
-If you have any questions, ideas or suggestions, please feel free to contact me at <a href='http://www.twitter.com/acasquetenotes'>X@acasquetenotes</a>
-or open an issue on GitHub at <a href='http://www.github.com/acasquete/taskmeapp/issues'>www.github.com/acasquete/taskmeapp</a>
-            `;
+            <div style="text-align: center;">
+                You're the best!<br/>
+                Thanks for trying <b>TaskMe</b>,<br/>
+                the <em>easiest</em> way to create notes!
+            </div>
+            <br/>
+            - <b>Create</b> a note by selecting a color on the left of the screen.<br/>
+            - <b>Edit</b> a note by double-clicking on it.<br/>
+            - <b>Remove</b> a note by dragging it to the top of the screen.<br/><br/>
+            If you have any questions, ideas or suggestions, please feel free to contact me at <a href='http://www.twitter.com/acasquetenotes'>X@acasquetenotes</a>
+            or open an issue on GitHub at <a href='http://www.github.com/acasquete/taskmeapp/issues'>www.github.com/acasquete/taskmeapp</a>
+        `;
 
         $(".tomato").remove();
         $('.note').fadeIn();
@@ -132,8 +132,9 @@ or open an issue on GitHub at <a href='http://www.github.com/acasquete/taskmeapp
         }
     }
 
-    function createNotes(notes) {
+    function initNotes(notes) {
         for (var i = 0; i < notes.length; i++) {
+            console.log(notes[i].l);
             createNote(notes[i].l, notes[i].t, notes[i].cl, notes[i].c);
             if (z < notes[i].i) z = notes[i].i;
         }
@@ -181,12 +182,8 @@ or open an issue on GitHub at <a href='http://www.github.com/acasquete/taskmeapp
     }
 
     function applyRandomRotate(element) {
-        var angle = getRandomAngle();
+        var angle = (Math.floor((Math.random() * 6) + 1)) - 3;
         element.style.transform = 'rotate(' + angle + 'deg)';
-    }
-
-    function getRandomAngle() {
-        return (Math.floor((Math.random() * 6) + 1)) - 3;
     }
 
     function starthandlers(element) {
@@ -267,19 +264,9 @@ or open an issue on GitHub at <a href='http://www.github.com/acasquete/taskmeapp
         return false;
     }
 
-    function getNotesInProgress() {
-        var notesinprogress = [];
-        var widthcolumn = screenwidth / 3;
-        $('.note').each(function () {
-            var el = $(this).get(0);
-            var position = $(this).position().left;
-            notesinprogress.push({ status: position > widthcolumn * 2 ? "done" : position > widthcolumn ? "inprogress" : "todo", text: el.innerHTML });
-        });
-        return notesinprogress;
-    }
-
     function saveTaskboard() {
         var listnotes = [];
+
         $('.note').each(function () {
             var el = $(this).get(0);
             listnotes.push({ c: el.innerHTML, i: el.style.zIndex, l: el.style.left, t: el.style.top, cl: el.className });
@@ -289,14 +276,21 @@ or open an issue on GitHub at <a href='http://www.github.com/acasquete/taskmeapp
     }
 
     function recalcposition() {
-        
-        $(".note").each(function (el) {
-            var posnote = $(this).position().left;
-            var newpos = window.innerWidth * posnote / screenwidth;
-            //$(this).css('left', newpos);
+        $(".note").each(function () {
+            var el = $(this).get(0);
+            el.style.transform = 'rotate(' + 0 + 'deg)';
+
+            var posNote = $(this).position().left;
+            var posRel = posNote * 100 / screenwidth;
+    
+            var newPos = window.innerWidth * posRel / 100;
+            $(this).css('left', newPos);
+            
         });
         screenwidth = window.innerWidth;
         saveTaskboard();
+
+        
     }
 
     function onViewStateChanged(eventArgs) {
