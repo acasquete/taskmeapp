8fc0e339422042ee1075323235253e0f6e385270
Alex Casquete <acasquete@plainconcepts.com>
2024-02-01 16:09:56 -0800

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Config } from "./services/ConfigService";
+import { Config } from "./services/ConfigService.ts";
 
 const configInstance = new Config();
 (window as any).Config = configInstance;
