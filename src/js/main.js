3442382691b1eeaf8ef09e64975e02a6aaab93dd
Alex Casquete <acasquete@plainconcepts.com>
2024-02-01 16:00:34 -0800

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,6 @@ import '../css/pomodoro.css';
 import '../css/fa/css/fontawesome.min.css';
 import '../css/fa/css/brands.min.css';
 import '../css/fa/css/solid.min.css';
-import '../services/configService.ts';
 import '../index.ts';
 import './taskboard.js';
 import './utils.js';
